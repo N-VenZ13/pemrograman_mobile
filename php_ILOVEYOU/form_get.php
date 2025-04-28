2dffad48950c1a0838c417888f42c75cecc85152
N-VenZ13 <staycool006@gmail.com>
2025-04-28 07:47:41 +0700

    upload project

---
+++ php_ILOVEYOU/form_get.php
@@ -0,0 +1,11 @@
+<?php
+    echo "Berikut Data Anda";
+    echo"<br/>";
+    echo "Nama: ".$_GET['nama'];
+    echo"<br/>";
+    echo "Alamat: ".$_GET['alamat'];
+    echo"<br/>";
+    echo "Email: ".$_GET['email'];
+    echo"<br/>";
+
+?>
